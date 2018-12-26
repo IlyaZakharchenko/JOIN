209bbd94821e4969dd4860dfd5e6fc1de9982b76
IlyaZakharchenko <zakharchenkoilyajob@gmail.com>
2018-12-26 04:35:49 +0300

    Added vacancies to project item

--- app/src/main/java/itis/ru/kpfu/join/ui/recyclerView/viewHolder/ProjectsViewHolder.kt
+++ app/src/main/java/itis/ru/kpfu/join/ui/recyclerView/viewHolder/ProjectsViewHolder.kt
@@ -1,8 +1,10 @@
 package itis.ru.kpfu.join.ui.recyclerView.viewHolder
 
+import android.support.design.chip.Chip
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import itis.ru.kpfu.join.api.model.Project
+import kotlinx.android.synthetic.main.item_project.view.cg_vacancies
 import kotlinx.android.synthetic.main.item_project.view.tv_project_description
 import kotlinx.android.synthetic.main.item_project.view.tv_project_name
 
@@ -11,6 +13,14 @@ class ProjectsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     fun bindViewHolder(item: Project, onClick: (Long) -> Unit) = with(itemView) {
         tv_project_name.text = item.name
         tv_project_description.text = item.description
+
+        item.vacancies?.forEach {
+            val chip = Chip(context)
+            chip.text = it.name
+            chip.chipEndPadding = 4.toFloat()
+            chip.chipStartPadding = 4.toFloat()
+            cg_vacancies.addView(chip)
+        }
 
         itemView.setOnClickListener { item.id?.let { it1 -> onClick(it1) } }
     }
